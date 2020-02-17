08d49048e3d500fd98a7b83a3ac311eb5c2acea6
Ankit Dimri <ankitdimri1999@gmail.com>
2020-02-17 17:30:29 +0530

    line plot for runs scored per over in the season

--- task2.R
+++ task2.R
@@ -1,6 +1,23 @@
+library (ggplot2)
 
 deliveries = read.csv2 ("datasets/task2/deliveries 1.csv")
 deliveries = as.data.frame (deliveries)
 deliveries
 
 summary (deliveries)
+
+deliveries$over = as.numeric (deliveries$over)
+deliveries$total_runs = as.numeric (deliveries$total_runs)
+deliveries$total_runs
+
+overs = unique (deliveries$over)
+total_runs = c ()
+for (over in overs) {
+  runs = deliveries [deliveries$over %in% over,"total_runs"]
+  total_runs = c(total_runs, sum (runs))
+}
+
+total_runs
+run_over = data.frame (over = overs, runs = total_runs)
+
+ggplot(run_over, aes(over,runs), las=2) + geom_line(color = "green")
