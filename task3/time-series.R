dd1d8ab085814b0979cdda34e4729104df10486c
Ankit Dimri <ankitdimri1999@gmail.com>
2020-04-14 19:31:33 +0530

    mean plot

--- task3/time-series.R
+++ task3/time-series.R
@@ -2,11 +2,11 @@
 im = read.csv2("imports.csv", sep = ",")
 im
 
-# Make time series variable out of it
+#2 Make time series variable out of it
 im = ts (im)
 plot (im [[, 2:ncol (im)]])
 
-# Plotting the yearly mean values
+#3 Plotting the yearly mean values
 mean = list ()
 for (i in (1:ceiling (nrow (im) /  12)))
   mean [[i]] = c (i)
@@ -35,3 +35,8 @@ for (i in (1:ceiling (nrow (im) /  12)))
 
 mean = ts (df [, 2:ncol (df)])
 plot (mean)
+
+
+
+#4 Plotting the yearly boxplots
+im.stl = stl (im, s.window = )
