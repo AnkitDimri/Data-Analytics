13d24c435c7f61c2fe13609b36ebfbf5c68dfd29
Ankit Dimri <ankitdimri1999@gmail.com>
2020-04-15 18:31:12 +0530

    ARIMA forecasting

--- task3/time-series.R
+++ task3/time-series.R
@@ -1,4 +1,6 @@
-library(forecast)
+library (forecast)
+library (ggplot2)
+library (Metrics)
 
 # import the dataset
 im = read.csv2("imports.csv", sep = ",")
@@ -54,12 +56,74 @@ plot (imports.residue)
 
 
 
-#8 Model using HoltWinters method
+#8 Model using HoltWinters method (train on 75% and predict 25%)
 imports.hw.train = window(imports,start = c(1,1) ,end=c(10,5))
 imports.hw.fc <- hw(imports.hw.train, seasonal = "additive", h = 54)
 
 summary (imports.hw.fc)
 plot (imports.hw.fc)
+
+
+
+
+
+#10 Plot the predicted values along with the actual values
+df = data.frame (imports.hw.fc , tail (imports, 54))
+
+timestamp = time (tail (imports, 54))
+plt = as.data.frame (data.frame (df$Point.Forecast, df$tail.imports..54.))
+ggplot (plt, aes (timestamp)) + geom_line(aes(y=dfplt$df.Point.Forecast),colour = "red")+
+  geom_line (aes (y = dfplt$df.tail.imports..54.), colour = "green") + xlab ("Time") + ylab ("Imports") + 
+  title ("Predicted (red) and actual (green) values graph")
+
+
+
+#11 Compute the rms error between the predicted and actual values.
+rmse (df$Point.Forecast, df$tail.imports..54.)
+
+
+#12 change alpha beta and gamma values to improve the model
+imports.hw.improved = HoltWinters (imports.hw.train, alpha = "0.5" ,beta = "0.5" ,gamma = "0.6" )
+
+model.predict = predict (imports.hw.improved, n.ahead = 54)
+round (model.predict)
+p_values= model.predict
+
+act_value = tail (imports, 54)
+
+rmse(act_value, p_values)
+
+# NO! even after trying for many values, can't get lower rms error
+
+
+
+
+
+#13 ARIMA model train
+imports.arima.train <- window (imports, start = c (1, 1) ,end = c (10, 5))
+model = auto.arima(imports.arima.train)
+model
+
+
+
+
+#14 Predict for next 25% data 
+imports.arima.predict <- forecast (model, h = 54)
+plot (imports.arima.predict)
+predicted = data.frame (imports.arima.predict)
+
+
+
+
+#Q15 Plotting actual and predicted values
+X = time (act_value)
+arima_df <- as.data.frame (data.frame (X, predicted$Point.Forecast, act_value))
+
+ggplot(arima_df,aes(X)) + geom_line (aes (y = predicted$Point.Forecast), colour = "red") +
+  geom_line (aes (y = act_value), colour = "green") + xlab("Time") + ylab("imports")
+
+
+
 
 
 
